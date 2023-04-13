65fa0c662cafd295a724a074baf83dbaeddae81c
joc <joc.mer@gmail.com>
2023-04-13 11:34:31 +0200

    adapt to docker compose v2

--- util/load_local_config.sh
+++ util/load_local_config.sh
@@ -8,6 +8,9 @@ function ased() {
   esac
 }
 
-export RCON_SECRET=$(ased -rn 's/^RCON_SECRET=([^\n]+)$/\1/p' local-config.properties)
-export MC_MAIN_PORT=$(ased -rn 's/^MC_MAIN_PORT=([^\n]+)$/\1/p' local-config.properties)
-export MC_RCON_PORT=$(ased -rn 's/^MC_RCON_PORT=([^\n]+)$/\1/p' local-config.properties)
+function load_local_config() {
+  export RCON_SECRET=$(ased -rn 's/^RCON_SECRET=([^\n]+)$/\1/p' local-config.properties)
+  export MC_MAIN_PORT=$(ased -rn 's/^MC_MAIN_PORT=([^\n]+)$/\1/p' local-config.properties)
+  export MC_RCON_PORT=$(ased -rn 's/^MC_RCON_PORT=([^\n]+)$/\1/p' local-config.properties)
+}
+
